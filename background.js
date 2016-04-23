67caa40278faeb9d15a0898d03f440fab562785a
AlvinHKH <email@alvinhkh.com>
2016-04-24 01:37:47 +0800

    remove live

--- background.js
+++ background.js
@@ -1,32 +1,32 @@
-function getNewURL(url) {
-    var layerId = 4; // 1-5
-    var matches = [
-        new RegExp("\/Stream\\(0[1-5^" + layerId + "]\\)\/","g"),
-        new RegExp("_Layer[1-5^" + layerId + "]_vod\.m3u8","g"),
-        new RegExp("\/Layer[1-5^" + layerId + "]\/","g"),
-        new RegExp("\/stream[1-5^" + layerId + "]\/","g"),
-    ];
-    var replaces = [
-        "/Stream(0" + layerId + ")/",
-        "_Layer" + layerId + "_vod.m3u8",
-        "/Layer" + layerId + "/",
-        "/stream" + layerId + "/"
-    ];
-    for (index = 0; index < matches.length; ++index) { 
-        if (replaces[index] != null && url.match(matches[index])) {
-            return url.replace(matches[index], replaces[index]);
-        }
+function getNewUrl(url) {
+  var matches = [
+    new RegExp("\/Stream\\(0[1235]\\)\/","g"),
+    new RegExp("_Layer[1235]_vod\\.m3u8","g"),
+    new RegExp("\/Layer[1235]\/","g"),
+    new RegExp("\/stream[1235]\/stream","g"),
+  ];
+  var replaces = [
+    "/Stream(04)/",
+    "_Layer4_vod.m3u8",
+    "/Layer4/",
+    "/stream4/stream"
+  ];
+  for (index = 0; index < matches.length; ++index) { 
+    if (replaces[index] != null && url.match(matches[index])) {
+      return {
+        redirectUrl: url.replace(matches[index], replaces[index])
+      };
     }
+  }
 }
 
+var requestFilter = {
+  urls: [
+    "*://*/session/*",
+    "*://nowtv100-i.akamaihd.net/hls/live/*"
+  ]
+};
+
 chrome.webRequest.onBeforeRequest.addListener(function (details) {
-    return {
-        redirectUrl: getNewURL(details.url)
-    };
-}, {
-    urls: [
-        "*://*/session/*",
-        "*://nowtv100-i.akamaihd.net/hls/live/*"
-    ],
-    tabId: -1
-}, ["blocking"]);
+  return getNewUrl(details.url);
+}, requestFilter, ["blocking"]);
