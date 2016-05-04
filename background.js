b8ebdc454a68eb7c09d7cba1a31b1c286263dc8f
AlvinHKH <email@alvinhkh.com>
2016-05-04 19:41:39 +0800

    remove live

--- background.js
+++ background.js
@@ -1,12 +1,10 @@
 function getNewUrl(url) {
   var matches = [
-    new RegExp("\/Stream\\(0[1235]\\)\/","g"),
     new RegExp("_Layer[1235]_vod\\.m3u8","g"),
     new RegExp("\/Layer[1235]\/","g"),
     new RegExp("\/stream[1235]\/stream","g"),
   ];
   var replaces = [
-    "/Stream(04)/",
     "_Layer4_vod.m3u8",
     "/Layer4/",
     "/stream4/stream"
@@ -23,7 +21,6 @@ function getNewUrl(url) {
 var requestFilter = {
   urls: [
     "*://*/session/*",
-    "*://nowtv100-i.akamaihd.net/hls/live/*",
     "*://*.viu.com/s/*"
   ]
 };
